fa5b7d3023cb1737be5ca7a7ca7ec67f3034d58f
PhysLi <2238661900@qq.com>
2025-11-29 22:48:37 +0800

    Commit the config and the temp json file

--- config.py
+++ config.py
@@ -9,13 +9,9 @@ OUTPUT_PATH = os.path.join(BASE_PATH, "output")
 
 # B站登录后获取的SESSDATA，CURRENT_QUALITY
 # 定期更换COOKIE的值即可
-COOKIE = 'buvid3=C5DA0AE5-EE6D-EC73-9C00-91643BB46A8508801infoc; b_nut=1663765808; i-wanna-go-back=-1; _uuid=FA89AA2B-DBC5-B651-3CC3-10108358FB358F11641infoc; buvid_fp_plain=undefined; DedeUserID=8366997; DedeUserID__ckMd5=b6567189d34e3723; nostalgia_conf=-1; b_ut=5; LIVE_BUVID=AUTO2616639434883138; hit-dyn-v2=1; buvid4=B22C1C1E-1184-C156-CE40-6069992045F409598-022092121-dbRG%2F63tuEZELCfw2OMgQw%3D%3D; hit-new-style-dyn=0; CURRENT_QUALITY=0; CURRENT_FNVAL=4048; rpdid=|(u|J|~~kJk~0J\'uYY)Yuuu|Y; SESSDATA=298698bb%2C1685424266%2Cbaba2%2Ac2; bili_jct=fff2e0e3ba510b35980175c124d37003; blackside_state=1; bp_article_offset_8366997=738033208792186900; fingerprint=ba212c0b9b265f06e25f216357e01581; buvid_fp=1ce661df5c741a8eb7525c6ae0aa0af3; sid=7s9j100c; PVID=1; theme_style=light; b_lsid=9B8A91027_1851AECBF75; bp_video_offset_8366997=740260806485082200; _dfcaptcha=535b285812d02dccfee87a4f182c7bfb; innersign=1'
+COOKIE = 'enable_web_push=DISABLE; DedeUserID=628798162; DedeUserID__ckMd5=605ead5cb800a4c2; buvid_fp_plain=undefined; CURRENT_BLACKGAP=0; fingerprint=4424f22474243a774b09e01d3f4a9fd9; buvid_fp=4424f22474243a774b09e01d3f4a9fd9; enable_feed_channel=ENABLE; header_theme_version=OPEN; theme-tip-show=SHOWED; theme-avatar-tip-show=SHOWED; buvid4=13F1AE8B-7014-A8C7-2771-FEA0E79672AE29687-024091912-z/2NJuNlxjzkI1rXp6siyFOi63xXGqRD919BkRum+weAk1LtB4N5WoSL8zLI/37D; buvid3=EDDAD8BE-A773-CCAB-E914-3EAED568BE6E38705infoc; b_nut=1758287838; _uuid=B210DB5D1-6D98-18D2-F287-5937655964D143034infoc; hit-dyn-v2=1; rpdid=0zbf5KI0AJ|12WuhEWts|1F|3w1VcPJ0; LIVE_BUVID=AUTO5617619174864305; PVID=2; theme-switch-show=SHOWED; home_feed_column=5; browser_resolution=1652-913; ogv_device_support_hdr=0; CURRENT_QUALITY=120; selectedMode=false; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjQ1MDY1NzgsImlhdCI6MTc2NDI0NzMxOCwicGx0IjotMX0.GucGNY_qgODr1I05_186nhSGGULRV4Ci3scIWQUGvV8; bili_ticket_expires=1764506518; SESSDATA=10da1bcf%2C1779799379%2Ced0a5%2Ab2CjCJM9GdqUTpEBWY4D4S5WIl70KXxChDUMXVoKP01QfYsREtfxdhML2XuouXJYKpyNYSVlB4ZkZFY1JlVWlHanotTzFIY3ZzVV9KQTJCVjhMcGlIRTQyeHpKNkVkVm1OUE11aVpkZ1BoSG1pbmt0OUpwRGpZVy16Q0lIY2Nua3N5anRaOHRmNXNnIIEC; bili_jct=94b135657c2bd65caf5caf4b2493b856; bp_t_offset_628798162=1139922889164718080; b_lsid=E87361CA_19AC59767D0; bmg_af_switch=1; bmg_src_def_domain=i2.hdslb.com; CURRENT_FNVAL=4048; sid=5udzg0du'
 
 # 下载视频的 URL
 URL = [
-    'https://www.bilibili.com/video/BV1M4411c7P4/?spm_id_from=333.999.0.0&vd_source=9c3224b88b8a3c4cc210fc6ff9b28f63',
-    'https://www.bilibili.com/video/BV1hB4y147j8/?spm_id_from=333.337.search-card.all.click',
-    # 分 P 视频示例
-    'https://www.bilibili.com/video/BV1TnsZzHEcz?spm_id_from=333.788.videopod.episodes&vd_source=9c3224b88b8a3c4cc210fc6ff9b28f63',
-    'https://www.bilibili.com/video/BV1TnsZzHEcz?spm_id_from=333.788.videopod.episodes&vd_source=9c3224b88b8a3c4cc210fc6ff9b28f63&p=2'
+    'https://www.bilibili.com/bangumi/play/ss39429?theme=movie&spm_id_from=333.337.0.0'
 ]
